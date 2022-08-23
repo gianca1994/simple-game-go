cded070ddf6c303181837ab1b6778b153c0c049f
Giancarlo <g.galvarini@alumno.um.edu.ar>
2022-08-23 09:54:57 -0300

    refactor userModel

--- src/model/User.go
+++ src/model/User.go
@@ -6,11 +6,15 @@ import (
 
 type User struct {
 	gorm.Model
-	Id         int    `gorm:"primary_key" json:"id"`
-	Name       string `gorm:"size:255;not null;unique" json:"name"`
-	DamageMax  int    `gorm:"not null" json:"damage_max"`
-	DamageMin  int    `gorm:"not null" json:"damage_min"`
-	Health     int    `gorm:"not null" json:"health"`
-	DefenseMax int    `gorm:"not null" json:"defense_max"`
-	DefenseMin int    `gorm:"not null" json:"defense_min"`
+	Id                    int    `gorm:"primary_key" json:"id"`
+	Username              string `gorm:"type:varchar(100);unique_index" json:"username"`
+	Password              string `gorm:"type:varchar(255)" json:"-"`
+	Level                 int    `gorm:"not null" json:"level"`
+	Experience            int    `gorm:"not null" json:"experience"`
+	ExperienceToNextLevel int    `gorm:"not null" json:"experience_to_next_level"`
+	DamageMax             int    `gorm:"not null" json:"damage_max"`
+	DamageMin             int    `gorm:"not null" json:"damage_min"`
+	Health                int    `gorm:"not null" json:"health"`
+	DefenseMax            int    `gorm:"not null" json:"defense_max"`
+	DefenseMin            int    `gorm:"not null" json:"defense_min"`
 }
